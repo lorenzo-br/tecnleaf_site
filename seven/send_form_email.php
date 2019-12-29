92f9a8339d1bfcb5c76bdb97a31a16df3b8adea4
unknown <Kennydouglas>
2019-12-29 19:19:31 -0200

    adicionado .br no email.

--- seven/send_form_email.php
+++ seven/send_form_email.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "tecnleaf@tecnleaf.com";
+    $email_to = "tecnleaf@tecnleaf.com.br";
     $email_subject = "tecnleaf";
  
     function died($error) {
